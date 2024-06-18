ac8d41e547e3cc2e5ba4dacf4cc54e2a19aa651d
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-18 18:17:42 -0300

    Configurando cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "7pgnhh",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     video:true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestam: "mmddyyyy_HHMMss"
+    }
   },
 });
